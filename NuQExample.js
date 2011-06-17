c0d073f15f56f116c510c5234fde0017ee3960ff
nka11 <nka11@nka.me>
2011-06-17 17:28:19 +0200

    Created first vowjs testsuite

--- NuQExample.js
+++ NuQExample.js
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Nu-Q.  If not, see <http://www.gnu.org/licenses/>.
  */
-var core = require("./NuQCore.js"), a,
+var core = require("./src/NuQCore.js"), a,
 	/**
 	 * Subclass implementation
 	 */
---
+++ test/APITest.js
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Nu-Q.
+ *
+ * Nu-Q is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Nu-Q is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Nu-Q.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var vows = require('vows'),
+    assert = require('assert'),
+    core = require("../src/NuQCore.js"),
+    _ = require('util'),
+    a, MyRepository,
+/**
+ * Subclass implementation
+ */
+MyNode = function() {
+	/**
+	 *  Below is an override example
+	 */
+	this.accept = function() {
+	    _.log("Overrides accept");
+    };
+    // Constructor of subclass
+    _.log("Subclass constructor");
+    _.log("`this` in contructor : ");
+    console.log(this);
+    _.log("`MyNode.super_` in contructor : ");
+    // MyNode._super access superclass's type prototype
+    console.log(MyNode.super_.constructor);
+    // 
+    // How to call super (as core.Node is an interface, this cannot be done here
+    //MyNode.super_.constructor.apply(this, arguments);
+    
+};
+_.inherits(MyNode,core.Node);
+_ = require('util');
+
+exports.suite1 = vows.describe('Nu-Q core API validation').addBatch({
+	'Testing a custom implementation of Node': {
+        topic: function() {
+        	return new MyNode();
+        },
+        'instanceof NuQItem': function(topic) {
+        	assert.ok((topic instanceof core.Item),'topic is not instanceOf NuQItem');
+        },
+        'instanceof NuQNode': function(topic) {
+        	assert.ok(topic instanceof core.Node,'topic is not instanceOf NuQNode');
+        },
+        'instanceof MyNode': function(topic) {
+        	assert.ok(topic instanceof MyNode,'topic is not instanceOf MyNode');
+        },
+        "Parent's unimplemented method throws exception": function(topic) {
+        	assert.throws(function() {
+        		topic.getProperties();
+        	}, Error);
+        },
+        "Parent of Parent's unimplemented method throws exception": function(topic) {
+        	assert.throws(function() {
+        		topic.getName();
+        	}, Error);
+        }
+    }
+});
